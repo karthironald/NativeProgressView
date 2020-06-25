a1852a5a13d72049f7d2f5ce9f768e00bad7e90c
Karthick Selvaraj <karthick@mallow-tech.com>
2020-06-25 09:21:35 +0530

    Initial commit.

---
+++ NativeProgressView/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NativeProgressView
+//
+//  Created by Karthick Selvaraj on 25/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NativeProgressView/NativeProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeProgressViewApp.swift
+//  NativeProgressView
+//
+//  Created by Karthick Selvaraj on 25/06/20.
+//
+
+import SwiftUI
+
+@main
+struct NativeProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
